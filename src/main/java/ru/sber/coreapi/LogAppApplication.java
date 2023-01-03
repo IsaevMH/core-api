b0bb3b7f5fec123d5249ed0112ef526857e0c4a8
Maxim_Isaev <iwork30rus@gmail.com>
2023-01-03 12:25:20 +0400

    Реализация слоя db.
    	- Добавлен шаг миграции;
    	- Добавлены entity Log и репозиторий для работы с сущностью на уровен БД.
    
    issues_#2

--- src/main/java/ru/sber/coreapi/LogAppApplication.java
+++ src/main/java/ru/sber/coreapi/LogAppApplication.java
@@ -1,8 +1,10 @@
-package ru.sber.logapp;
+package ru.sber.coreapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = {"ru.sber.coreapi.repository"})
 @SpringBootApplication
 public class LogAppApplication {
 
